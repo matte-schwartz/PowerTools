dd574af087a3ca1e53bdc5687e7bcfee17a4bddf
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-09-22 20:17:01 -0700

    Update gpu.tsx

--- src/components/gpu.tsx
+++ src/components/gpu.tsx
@@ -11,169 +11,182 @@ import {
 } from "decky-frontend-lib";
 import * as backend from "../backend";
 import { tr } from "usdpl-front";
-import {
-    LIMITS_INFO,
-    SLOW_PPT_GPU,
-    FAST_PPT_GPU,
-    TDP,
-    PRESET_MODE_GPU,
-} from "../consts";
+import { LIMITS_INFO, SLOW_PPT_GPU, FAST_PPT_GPU } from "../consts";
 import { set_value, get_value } from "usdpl-front";
 
-export class Gpu extends Component<backend.IdcProps> {
-    constructor(props: backend.IdcProps) {
-        super(props);
-        this.state = {
-            reloadThingy: "/shrug",
-        };
-    }
+const PRESET_MODE_GPU = {
+    silent: {
+        tdp: 10,
+        ppt_slow: SLOW_PPT_GPU,
+        ppt_fast: SLOW_PPT_GPU,
+    },
+    performance: {
+        tdp: 15,
+        ppt_slow: SLOW_PPT_GPU,
+        ppt_fast: FAST_PPT_GPU,
+    },
+    turbo25: {
+        tdp: 25,
+        ppt_slow: SLOW_PPT_GPU,
+        ppt_fast: FAST_PPT_GPU,
+    },
+    turbo30: {
+        tdp: 30,
+        ppt_slow: SLOW_PPT_GPU,
+        ppt_fast: FAST_PPT_GPU,
+    },
+    custom: {
+        tdp: get_value("gpu_tdp"),
+        ppt_slow: get_value("gpu_ppt_slow"),
+        ppt_fast: get_value("gpu_ppt_fast"),
+    },
+    "Performance 20W": {
+        tdp: 20,
+        ppt_slow: SLOW_PPT_GPU,
+        ppt_fast: FAST_PPT_GPU,
+    },
+    "40W Overdrive": {
+        tdp: 40,
+        ppt_slow: 45,
+        ppt_fast: 53,
+    },
+};
+
+export class Gpu extends Component {
+    handlePerformanceChange = (value: number) => {
+        switch (value) {
+            case 0:
+                set_value("gpu_tdp", PRESET_MODE_GPU.silent.tdp);
+                set_value("gpu_ppt_slow", PRESET_MODE_GPU.silent.ppt_slow);
+                set_value("gpu_ppt_fast", PRESET_MODE_GPU.silent.ppt_fast);
+                break;
+            case 1:
+                set_value("gpu_tdp", PRESET_MODE_GPU.performance.tdp);
+                set_value("gpu_ppt_slow", PRESET_MODE_GPU.performance.ppt_slow);
+                set_value("gpu_ppt_fast", PRESET_MODE_GPU.performance.ppt_fast);
+                break;
+            case 2:
+                set_value("gpu_tdp", PRESET_MODE_GPU.turbo25.tdp);
+                set_value("gpu_ppt_slow", PRESET_MODE_GPU.turbo25.ppt_slow);
+                set_value("gpu_ppt_fast", PRESET_MODE_GPU.turbo25.ppt_fast);
+                break;
+            case 3:
+                set_value("gpu_tdp", PRESET_MODE_GPU.turbo30.tdp);
+                set_value("gpu_ppt_slow", PRESET_MODE_GPU.turbo30.ppt_slow);
+                set_value("gpu_ppt_fast", PRESET_MODE_GPU.turbo30.ppt_fast);
+                break;
+            case 4:
+                set_value("gpu_tdp", PRESET_MODE_GPU.custom.tdp);
+                set_value("gpu_ppt_slow", PRESET_MODE_GPU.custom.ppt_slow);
+                set_value("gpu_ppt_fast", PRESET_MODE_GPU.custom.ppt_fast);
+                break;
+            case 5:
+                set_value("gpu_tdp", PRESET_MODE_GPU["Performance 20W"].tdp);
+                set_value("gpu_ppt_slow", PRESET_MODE_GPU["Performance 20W"].ppt_slow);
+                set_value("gpu_ppt_fast", PRESET_MODE_GPU["Performance 20W"].ppt_fast);
+                break;
+            case 6:
+                set_value("gpu_tdp", PRESET_MODE_GPU["40W Overdrive"].tdp);
+                set_value("gpu_ppt_slow", PRESET_MODE_GPU["40W Overdrive"].ppt_slow);
+                set_value("gpu_ppt_fast", PRESET_MODE_GPU["40W Overdrive"].ppt_fast);
+                break;
+        }
+    };
 
     render() {
         const reloadGUI = (x: string) => this.setState({ reloadThingy: x });
 
         const performanceDropdown: SingleDropdownOption[] = [
-            {data: 0, label: <span>Silent 10W</span> }, 
-            {data: 1, label: <span>Peformance 15W</span> }, 
-            {data: 5, label: <span>Performance 20W</span>},
-            {data: 2, label: <span>Turbo 25W</span>}, 
-            {data: 3, label: <span>Turbo 30W</span>}, 
-            {data: 4, label: <span>Manual</span>}
+            { data: 0, label: <span>Silent 10W</span> },
+            { data: 1, label: <span>Peformance 15W</span> },
+            { data: 5, label: <span>Performance 20W</span> },
+            { data: 2, label: <span>Turbo 25W</span> },
+            { data: 3, label: <span>Turbo 30W</span> },
+            { data: 4, label: <span>Manual</span> },
+            { data: 6, label: <span>40W Overdrive</span> },
         ];
 
-        const labels : string [] = ["Silent 10W", "Performance 15W", "Turbo 25W", "Turbo 30W", "Manual", "Performance 20W"];
+        const labels: string[] = [
+            "Silent 10W",
+            "Performance 15W",
+            "Turbo 25W",
+            "Turbo 30W",
+            "Manual",
+            "Performance 20W",
+            "40W Overdrive",
+        ];
 
-        return (<Fragment>
-            {/* GPU */}
-            <div className={staticClasses.PanelSectionTitle}>
-                {tr("GPU")}
-            </div>
-            {((get_value(LIMITS_INFO) as backend.SettingsLimits).gpu.fast_ppt_limits != null || (get_value(LIMITS_INFO) as backend.SettingsLimits).gpu.slow_ppt_limits != null) && <PanelSectionRow>
-                <ToggleField
-                    checked={get_value(SLOW_PPT_GPU) != null || get_value(FAST_PPT_GPU) != null}
-                    label={tr("Thermal Power (TDP) Limit")}
-                    description={tr("Limits processor power for less total power")}
-                    onChange={(value: boolean) => {
-                        if (value) {
-                            if ((get_value(LIMITS_INFO) as backend.SettingsLimits).gpu.slow_ppt_limits != null) {
-                                set_value(SLOW_PPT_GPU, (get_value(LIMITS_INFO) as backend.SettingsLimits).gpu.slow_ppt_limits!.max);
-
-                                // Set it to midpoint
-                                set_value(TDP, (get_value(LIMITS_INFO) as backend.SettingsLimits).gpu.slow_ppt_limits!.max / 2000);
-                            }
-
-                            if ((get_value(LIMITS_INFO) as backend.SettingsLimits).gpu.fast_ppt_limits != null) {
-                                set_value(FAST_PPT_GPU, (get_value(LIMITS_INFO) as backend.SettingsLimits).gpu.fast_ppt_limits!.max);
-                            }
-                            reloadGUI("GPUPPTToggle");
-                        } else {
-                            set_value(SLOW_PPT_GPU, null);
-                            set_value(FAST_PPT_GPU, null);
-                            set_value(TDP, null);
-                            backend.resolve(backend.unsetGpuPpt(), (_: any[]) => {
-                                reloadGUI("GPUUnsetPPT");
-                            });
-                        }
-                    }}
-                />
-            </PanelSectionRow>}
-            <PanelSectionRow>
-                    <Field
-                    label={tr("Performance Preset")}
-                    >
-                    <Dropdown
-                        menuLabel={tr("Performance")}
-                        rgOptions={performanceDropdown}
-                        selectedOption={performanceDropdown.find((val: SingleDropdownOption, _index, _arr) => {
-                            backend.log(backend.LogLevel.Info, "Checking selected option: " + val.data.toString());
-
-                            // Default to Performance 15W
-                            if (get_value(PRESET_MODE_GPU) == null) {
-                                backend.log(backend.LogLevel.Info, "Preset mode is null");
-                                return val.data == 1;
-                            } else {
-                                backend.log(backend.LogLevel.Info, "Preset mode is " + get_value(PRESET_MODE_GPU).toString());
-                            }
-                            return val.data == get_value(PRESET_MODE_GPU);
-                        })}
-                        strDefaultLabel={get_value(PRESET_MODE_GPU) == null ? "Performance 15W" : labels[get_value(PRESET_MODE_GPU)]}
-                        onChange={(elem: SingleDropdownOption) => {
-                            backend.log(backend.LogLevel.Debug, "Performance dropdown selected " + elem.data.toString());
-                            backend.resolve(backend.setPreset(elem.data), (_value) => {});
-
-                            // Set this outside instead of in the callback because it's faster for UI.
-                            backend.log(backend.LogLevel.Info, "Preset mode is now " + elem.data.toString());
-                            set_value(PRESET_MODE_GPU, elem.data);
-
-                            switch (elem.data) {
-                                case 0:
-                                    backend.resolve(backend.setGpuPptTdp(10000, 17000, 14000), (limits: number[]) => {
-                                        set_value(TDP, limits[0]/1000);
-                                        set_value(FAST_PPT_GPU, limits[1]);
-                                        set_value(SLOW_PPT_GPU, limits[2]);
-                                    });
-                                    break;
-                                case 1:
-                                    backend.resolve(backend.setGpuPptTdp(15000, 25000, 20000), (limits: number[]) => {
-                                        set_value(TDP, limits[0]/1000);
-                                        set_value(FAST_PPT_GPU, limits[1]);
-                                        set_value(SLOW_PPT_GPU, limits[2]);
-                                    });
-                                    break;
-                                case 2:
-                                    backend.resolve(backend.setGpuPptTdp(25000, 35000, 30000), (limits: number[]) => {
-                                        set_value(TDP, limits[0]/1000);
-                                        set_value(FAST_PPT_GPU, limits[1]);
-                                        set_value(SLOW_PPT_GPU, limits[2]);
-                                    });
-                                    break;
-                                case 5:
-                                    backend.resolve(backend.setGpuPptTdp(20000, 30000, 25000), (limits: number[]) => {
-                                        set_value(TDP, limits[0]/1000);
-                                        set_value(FAST_PPT_GPU, limits[1]);
-                                        set_value(SLOW_PPT_GPU, limits[2]);
-                                    });
-                                    break;
-                                case 3:
-                                    backend.resolve(backend.setGpuPptTdp(30000, 53000, 43000), (limits: number[]) => {
-                                        set_value(TDP, limits[0]/1000);
-                                        set_value(FAST_PPT_GPU, limits[1]);
-                                        set_value(SLOW_PPT_GPU, limits[2]);
-                                    });
-                                    break;
-                                case 4:
-                                    break;
-                            }
-
-                            reloadGUI("performance " + (new Date()).getTime().toString());
-                        }}
+        return (
+            <Fragment>
+                {/* GPU */}
+                <div className={staticClasses.PanelSectionTitle}>
+                    {tr("GPU")}
+                </div>
+                <PanelSectionRow>
+                    <ToggleField
+                        label={tr("Enable GPU Tuning")}
+                        field={backend.gpu_tuning_enabled}
                     />
+                </PanelSectionRow>
+                <PanelSectionRow>
+                    <Field label={tr("TDP Limit (W)")}>
+                        <SliderField
+                            field={backend.gpu_tdp}
+                            min={10}
+                            max={LIMITS_INFO.gpu_tdp_max}
+                            step={1}
+                            marks={{
+                                10: 10,
+                                [LIMITS_INFO.gpu_tdp_max]: LIMITS_INFO.gpu_tdp_max,
+                            }}
+                        />
                     </Field>
-            </PanelSectionRow>
-            <PanelSectionRow>
-                {get_value(TDP) != null && get_value(PRESET_MODE_GPU) == 4 && <SliderField
-                    label={tr("Watts")}
-                    value={get_value(TDP)}
-                    max={30}
-                    min={7}
-                    step={1}
-                    showValue={true}
-                    disabled={get_value(TDP) == null || get_value(PRESET_MODE_GPU) != 4}
-                    onChange={(tdp: number) => {
-                        backend.log(backend.LogLevel.Debug, "TDP is now " + tdp.toString());
-                        const oldTDP = get_value(TDP);
-                        const newTdp = tdp;
-                        if (newTdp != oldTDP) {
-                            backend.resolve(backend.setGpuPptTdp(newTdp * 1000, newTdp * 1000 + 2000, newTdp * 1000),
-                                (limits: number[]) => {
-                                    set_value(TDP, limits[0] / 1000);
-                                    set_value(FAST_PPT_GPU, limits[1]);
-                                    set_value(SLOW_PPT_GPU, limits[2]);
-                                    reloadGUI("GPUTDP");
-                                });
-                        }
-                    }}
-                />}
-            </PanelSectionRow>
-        </Fragment>);
+                </PanelSectionRow>
+                <PanelSectionRow>
+                    <Field label={tr("Performance Preset")} className={staticClasses.w100}>
+                        <Dropdown
+                            value={get_value("gpu_preset")}
+                            onChange={this.handlePerformanceChange}
+                        >
+                            {performanceDropdown.map((option, index) => (
+                                <SingleDropdownOption
+                                    key={option.data}
+                                    value={option.data}
+                                    label={option.label}
+                                />
+                            ))}
+                        </Dropdown>
+                    </Field>
+                </PanelSectionRow>
+                <PanelSectionRow>
+                    <Field label={tr("Slow PPT Limit (W)")}>
+                        <SliderField
+                            field={backend.gpu_ppt_slow}
+                            min={SLOW_PPT_GPU}
+                            max={FAST_PPT_GPU}
+                            step={1}
+                            marks={{
+                                [SLOW_PPT_GPU]: SLOW_PPT_GPU,
+                                [FAST_PPT_GPU]: FAST_PPT_GPU,
+                            }}
+                        />
+                    </Field>
+                </PanelSectionRow>
+                <PanelSectionRow>
+                    <Field label={tr("Fast PPT Limit (W)")}>
+                        <SliderField
+                            field={backend.gpu_ppt_fast}
+                            min={SLOW_PPT_GPU}
+                            max={FAST_PPT_GPU}
+                            step={1}
+                            marks={{
+                                [SLOW_PPT_GPU]: SLOW_PPT_GPU,
+                                [FAST_PPT_GPU]: FAST_PPT_GPU,
+                            }}
+                        />
+                    </Field>
+                </PanelSectionRow>
+            </Fragment>
+        );
     }
 }
