826ca141c0e7e118f3f7cd3f3f60f8ecfd5cc5b2
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-09-24 09:07:01 -0700

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -8,5 +8,5 @@ cargo build --release
 mkdir -p ../bin
 #cp --preserve=mode ./target/x86_64-unknown-linux-musl/release/powertools ../bin/backend
 #cp --preserve=mode ./target/x86_64-unknown-linux-musl/debug/powertools ../bin/backend
-#cp --preserve=mode ./target/debug/powertools ../bin/backend
-cp --preserve=mode ./target/debug/powertools ../bin/backend
+#cp --preserve=mode ./target/release/powertools ../bin/backend
+cp --preserve=mode ./target/release/powertools ../bin/backend
